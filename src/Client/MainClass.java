348049b4da67081cd3503fec72d1d92b7a3a2640
Tyler Coverstone <coverstonet@gmail.com>
2017-05-21 17:01:11 -0400

    Update MainClass.java
    This might work

--- src/Client/MainClass.java
+++ src/Client/MainClass.java
@@ -15,7 +15,30 @@ public class MainClass
 {
 	public static void main(String[] args)
    {
-		boolean done = false;
+      System.out.println("How many users would you like to simulate at one time?");
+      
+      Scanner users = new Scanner(System.in);
+      
+      boolean correctUsers = false;
+      int numUsers = 1;
+      
+      while(!correctUsers)
+      {
+         if(users.hasNextInt())
+         {
+            numUsers = users.nextInt();
+            correctUsers = true;
+         }
+         else
+            System.out.print("Input was not a number, please try again");  
+      }
+      
+      if(numUsers == 1)
+         singleUser();
+      else
+         multipleUsers();
+      
+		/*boolean done = false;
       
       while(!done)
       {
@@ -83,7 +106,112 @@ public class MainClass
                break;           
          }
          
-      }
+      }*/
+   }
+   
+   public static void singleUser()
+   {
+      try 
+       (
+	       Socket kkSocket = new Socket("127.0.0.1", 8080);
+	       PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
+	       BufferedReader in = new BufferedReader(new InputStreamReader(kkSocket.getInputStream()));
+	    ) 
+       {
+         
+            boolean done = false;
+      
+         while(!done)
+         {
+             System.out.println("1. Host current Date and Time");
+             System.out.println("2. Host uptime");
+             System.out.println("3. Host memory use");
+             System.out.println("4. Host Netstat");
+             System.out.println("5. Host current users");
+             System.out.println("6. Host running processes");
+             System.out.println("7. Quit");
+         
+             System.out.println("");
+         
+             System.out.println("Choose an option from the above menu.");
+         
+             Scanner s = new Scanner(System.in);
+         
+             int userInpt = 0;
+             boolean correctInpt = false;
+         
+             while(!correctInpt)
+             {
+                  userInpt = s.nextInt();
+            
+                  if((userInpt == 1) || (userInpt == 2) || (userInpt == 3) || (userInpt == 4) || (userInpt == 5) || (userInpt == 6) || (userInpt == 7))
+                  {
+                      correctInpt = true;
+                  }
+                  else
+                  {
+                      System.out.println("Incorrect Input: Please try again");
+                  }
+             }
+         
+             String str = "err";
+         
+             switch(userInpt)
+             {
+               case 1:
+                   str = "time"; 
+                   break;
+               case 2:
+                   str = "uptime";
+                   break;
+               case 3:
+                   str = "memory";
+                   break;
+               case 4:
+                   str = "netstat";
+                   break;
+               case 5:
+                   str = "users";
+                   break;
+               case 6:
+                   str = "process";
+                   break;
+               case 7: 
+                   done = true;
+                   break;           
+             }
+         
+            if(done == false)
+            {
+	            if(in.readLine().equals("1"))
+               {
+                   out.println(str); 
+               }
+          
+               String receivedData = in.readLine();
+          
+               System.out.println(receivedData);
+          
+               kkSocket.close();
+            }
+          }
+	    } 
+       catch (UnknownHostException e) 
+       {
+	        System.err.println("Don't know about host ");
+	        System.exit(1);
+	    } 
+       catch (IOException e) 
+       {
+	        System.err.println("Couldn't get I/O for the connection to ");
+	        System.exit(1);
+	    }   
+
+   }
+   
+   public static void multipleUsers()
+   {
+   
    }
    
    public static void sendRequest(String s)
