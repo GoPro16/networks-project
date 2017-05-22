2322b39085189edc4deae1a0f32855447337d8f9
Tyler Coverstone <coverstonet@gmail.com>
2017-05-21 20:49:56 -0400

    Update MainClass.java
    First Final Draft

--- src/Client/MainClass.java
+++ src/Client/MainClass.java
@@ -7,6 +7,7 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
+import java.lang.Thread;
 
 /*
  * Main class for the client side of the program
@@ -36,77 +37,7 @@ public class MainClass
       if(numUsers == 1)
          singleUser();
       else
-         multipleUsers();
-      
-		/*boolean done = false;
-      
-      while(!done)
-      {
-         System.out.println("1. Host current Date and Time");
-         System.out.println("2. Host uptime");
-         System.out.println("3. Host memory use");
-         System.out.println("4. Host Netstat");
-         System.out.println("5. Host current users");
-         System.out.println("6. Host running processes");
-         System.out.println("7. Quit");
-         
-         System.out.println("");
-         
-         System.out.println("Choose an option from the above menu.");
-         
-         Scanner s = new Scanner(System.in);
-         
-         int userInpt = 0;
-         boolean correctInpt = false;
-         
-         while(!correctInpt)
-         {
-            userInpt = s.nextInt();
-            
-            if((userInpt == 1) || (userInpt == 2) || (userInpt == 3) || (userInpt == 4) || (userInpt == 5) || (userInpt == 6) || (userInpt == 7))
-            {
-               correctInpt = true;
-            }
-            else
-            {
-               System.out.println("Incorrect Input: Please try again");
-            }
-         }
-         
-         String str;
-         
-         switch(userInpt)
-         {
-            case 1:
-               str = "time"; 
-               sendRequest(str);
-               break;
-            case 2:
-               str = "uptime";
-               sendRequest(str);
-               break;
-            case 3:
-               str = "memory";
-               sendRequest(str);
-               break;
-            case 4:
-               str = "netstat";
-               sendRequest(str);
-               break;
-            case 5:
-               str = "users";
-               sendRequest(str);
-               break;
-            case 6:
-               str = "process";
-               sendRequest(str);
-               break;
-            case 7: 
-               done = true;
-               break;           
-         }
-         
-      }*/
+         multipleUsers(numUsers);
    }
    
    public static void singleUser()
@@ -178,7 +109,8 @@ public class MainClass
                    break;
                case 7: 
                    done = true;
-                   break;           
+                   kkSocket.close(); 
+                   break;               
              }
          
             if(done == false)
@@ -190,11 +122,11 @@ public class MainClass
           
                String receivedData = in.readLine();
           
-               System.out.println(receivedData);
-          
-               kkSocket.close();
+               System.out.println(receivedData); 
             }
           }
+          
+          
 	    } 
        catch (UnknownHostException e) 
        {
@@ -209,29 +141,124 @@ public class MainClass
 
    }
    
-   public static void multipleUsers()
+   public static void multipleUsers(int numUsers)
    {
-   
+      boolean done = false;
+      
+      while(!done)
+      {
+         System.out.println("1. Host current Date and Time");
+         System.out.println("2. Host uptime");
+         System.out.println("3. Host memory use");
+         System.out.println("4. Host Netstat");
+         System.out.println("5. Host current users");
+         System.out.println("6. Host running processes");
+         System.out.println("7. Quit");
+         
+         System.out.println("");
+         
+         System.out.println("Choose an option from the above menu.");
+         
+         Scanner s = new Scanner(System.in);
+         
+         int userInpt = 0;
+         boolean correctInpt = false;
+         
+         while(!correctInpt)
+         {
+            userInpt = s.nextInt();
+            
+            if((userInpt == 1) || (userInpt == 2) || (userInpt == 3) || (userInpt == 4) || (userInpt == 5) || (userInpt == 6) || (userInpt == 7))
+            {
+               correctInpt = true;
+            }
+            else
+            {
+               System.out.println("Incorrect Input: Please try again");
+            }
+         }
+         
+         String str = "err";
+         
+         switch(userInpt)
+         {
+            case 1:
+               str = "time"; 
+               break;
+            case 2:
+               str = "uptime";
+               break;
+            case 3:
+               str = "memory";
+               break;
+            case 4:
+               str = "netstat";
+               break;
+            case 5:
+               str = "users";
+               break;
+            case 6:
+               str = "process";
+               break;
+            case 7: 
+               done = true;
+               break;           
+         }
+         
+         TimeKeeper tk = new TimeKeeper();
+         
+         if(done == false)
+         {
+            UserThread[] threads = new UserThread[numUsers];
+      
+            for(int i=0; i<numUsers; i++)
+            {
+               threads[i] = new UserThread();
+            }
+      
+            for(int i=0; i<numUsers; i++)
+            {
+               threads[i].run(str, tk);
+            }
+         }
+         
+         long avgTime = tk.returnAverage();
+         System.out.println("Average time for thread execuation was " + avgTime); 
+         System.out.println("");     
+      }
+      
    }
-   
-   public static void sendRequest(String s)
-   {   
-	 
-	    try 
+}
+
+class UserThread extends Thread
+{
+   public void run(String str, TimeKeeper tk)
+   {
+      try 
        (
 	       Socket kkSocket = new Socket("127.0.0.1", 8080);
 	       PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
 	       BufferedReader in = new BufferedReader(new InputStreamReader(kkSocket.getInputStream()));
 	    ) 
        {
+          long startTime = 0;
+          long endTime = 0;
+          
 	       if(in.readLine().equals("1"))
           {
-              out.println(s); 
+              startTime = System.currentTimeMillis();
+              out.println(str); 
           }
           
           String receivedData = in.readLine();
+          endTime = System.currentTimeMillis();
           
           System.out.println(receivedData);
+          
+          long elapsedTime = endTime - startTime;
+          
+          tk.sumTime += elapsedTime;
+          tk.numTimes++;
           
           kkSocket.close();
 	    } 
@@ -244,6 +271,17 @@ public class MainClass
        {
 	        System.err.println("Couldn't get I/O for the connection to ");
 	        System.exit(1);
-	    }   
-	}
+	    }
+   }
+}
+
+class TimeKeeper
+{
+   public long sumTime = 0;
+   public long numTimes = 0;
+   
+   public long returnAverage()
+   {
+      return sumTime/numTimes;
+   }
 }
